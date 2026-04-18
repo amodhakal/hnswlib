049bca0378cdbdc226cfef32c73e15562d1b1389
Amodh Dhakal <amodhakal@gmail.com>
2026-04-18 01:30:42 -0400

    feat: setup insert declaration

--- include/hnswlib/hnswlib.h
+++ include/hnswlib/hnswlib.h
@@ -1,1 +1,18 @@
-class HNSW {};
+#include <sys/types.h>
+
+typedef uint HNSWElement;
+
+class HNSW
+{
+    /**
+    Insert a value into HNSW
+
+    @param newElement the new element (q)
+    @param establishedConnectionCount number of established connections (M)
+    @param maxConnectionEachElementPerLayer max number of connections for each element per layer (mmax)
+    @param dynamicCandidateListSize size of dynamic candidate list (efConstruction)
+    @param normalizatioNFactorLevelGeneration normalization factor for level generation (m)
+    */
+    void insert(HNSWElement newElement, uint establishedConnectionCount, uint maxConnectionEachElementPerLayer,
+                uint dynamicCandidateListSize, uint normalizatioNFactorLevelGeneration);
+};
---
+++ src/hnswlib.cpp
@@ -0,0 +1,6 @@
+#include <hnswlib/hnswlib.h>
+
+void HNSW::insert(HNSWElement newElement, uint establishedConnectionCount, uint maxConnectionEachElementPerLayer,
+                  uint dynamicCandidateListSize, uint normalizatioNFactorLevelGeneration)
+{
+}
